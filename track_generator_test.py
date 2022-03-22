0fb5acd6550a86e5fda76e2ba799251c7475ce95
dserafim1999 <daniel.serafim@tecnico.ulisboa.pt>
2022-03-22 15:49:39 +0000

    removed config.json from repository

--- track_generator_test.py
+++ track_generator_test.py
@@ -1,10 +1,7 @@
-from urllib.parse import urlencode, urlparse, parse_qsl
+from urllib.parse import urlencode
 import requests
 
 api_key = "AIzaSyB5t7hSzpEmx1hk6clwou9wdQCpIvpDvP8"
-
-data_type = 'json'
-
 
 
 def extract_lat_lng(address_or_postalcode, data_type = 'json'):
@@ -41,12 +38,29 @@ def get_route(start, end, data_type = 'json'):
 
     points = []
 
+
     for i in range(len(steps)):
         if (i == 0):
             points.append(steps[i]["start_location"])
         points.append(steps[i]["end_location"])
 
     return points
+
+def get_route_info(start, end, data_type = 'json'):
+    endpoint = f"https://maps.googleapis.com/maps/api/distancematrix/{data_type}"
+    params= {"destinations": start, "origins": end, "key": api_key}
+    url_params = urlencode(params)
+    url = f"{endpoint}?{url_params}"
+    r = requests.get(url)
+    result = {}
+    if r.status_code not in range(200, 299):
+        return {}
+    try:
+        result = r.json() 
+    except:
+        pass
+    print(url)
+    return result
 
 def indentation(n):
     return ''.join('\t' for i in range(n))
@@ -61,13 +75,14 @@ def point_gpx(point):
         '</trkpt>'
     ]) + '\n'
 
-def generate_gpx_file(start, end):
+def to_gpx(start, end):
     points = get_route(start, end)
 
     segments = ''.join([point_gpx(point) for point in points])
     
     return ''.join([
         '<?xml version="1.0" encoding="UTF-8"?>\n',
+        '<!-- -->\n'
         '<gpx xmlns="http://www.topografix.com/GPX/1/1">\n',
         indentation(1) + '<trk>\n',
         indentation(2) + '<trkseg>\n', 
@@ -76,27 +91,11 @@ def generate_gpx_file(start, end):
         indentation(1) + '</trk>\n',
         '</gpx>\n'
     ])
+
+def generate_gpx_file(start, end):
+    with open("tracks\\input\\test.gpx", "w+") as f:
+            f.write(to_gpx(start, end))
+            f.close()
     
-
-#const segments = state.get('tracks').get('tracks').get(trackId).get('segments').toJS().map((segmentId, i) => {
-#    const segment = state.get('tracks').get('segments').get(segmentId);
-#    const identation = '\t'.repeat(3);
-#    const points = segment.get('points').map((point) => {
-#      return [
-#        identation,
-#        '<trkpt lat="' + point.get('lat') + '" lon="' + point.get('lon') + '">',
-#        '<time>' + point.get('time').toISOString().replace(MS_REG, 'Z') + '</time>',
-#        '</trkpt>'
-#      ].join('');
-#    }).toJS().join('\n');
-#    return '\t\t<trkseg>\n' + points + '\n\t\t</trkseg>';
-#  }).join('\n');
-#  return [
-#    '<?xml version="1.0" encoding="UTF-8"?>',
-#    '<gpx xmlns="http://www.topografix.com/GPX/1/1">',
-#    '\t<trk>', segments, '\t</trk>',
-#    '</gpx>'
-#  ].join('\n');
-
 if __name__=="__main__":
-    print(generate_gpx_file("38.735986,-9.138793","38.7233,-9.1489"))
+    generate_gpx_file("Mc Donald's Santarém", "Vale de Santarém")
